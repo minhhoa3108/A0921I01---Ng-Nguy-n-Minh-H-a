ec2a3dc260dda803c59977eb79ebc4441c08cf40
minhhoa3108 <minhhoango448@gmail.com>
2021-12-03 18:34:30 +0700

    commit

---
+++ Module 2/exercise/src/main/java/com/example/untitled/UntitledRestApplication.java
@@ -0,0 +1,11 @@
+package com.example.untitled;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class UntitledRestApplication extends Application {
+}
